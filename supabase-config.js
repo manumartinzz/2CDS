098cb38c015f1065317f41fbba399c36997b5362
milenak <milena.kachimarck@escola.pr.gov.br>
2026-08-20 22:35:49 -0300

    banco de dados

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-    url: 'https://evmvxgwdtxzitjrfpcrp.supabase.co/rest/v1/',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2bXZ4Z3dkdHh6aXRqcmZwY3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjkzNDAsImV4cCI6MjEwMjgwNTM0MH0.yMDpRUJpSU60i6VF3Vclkf_rwgIhjrfy0sYf82rWpQA'
+    url: 'https://dkwucuzyqxbiydsitkco.supabase.co/rest/v1/',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrd3VjdXp5cXhiaXlkc2l0a2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyNzAzOTIsImV4cCI6MjEwMjg0NjM5Mn0.4RAqAIY-sOwQqkJyDujAhGl_83lcdLo-gPeSMi10eSk'
 };
